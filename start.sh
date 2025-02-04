6ba4cdd4b8b402b352709aa17c93abe42f933d10
Movic16 <ibrahim160198@hotmail.com>
2025-02-04 17:34:18 +0100

    lancement du jeux Ook

--- start.sh
+++ start.sh
@@ -1,4 +1,18 @@
 # Active l'environement virtuel
 
 # choix du port
-PORT=11020
+PORT=12345
+
+# Port du serial
+#SERIAL=COM8          # on Windows
+SERIAL=/dev/ttyUSB0  # on Linux
+
+# start the server
+python -m space_collector.game.server -p $PORT --timeout 10 &
+
+sleep 3
+
+# start the viewer / on small screens : --small-window
+python -m space_collector.viewer -p $PORT --small-window &
+
+python -m space_collector.serial2tcp -p $PORT --serial $SERIAL --team-name "USS-Discovery"
