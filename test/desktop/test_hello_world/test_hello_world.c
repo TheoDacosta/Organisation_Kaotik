8e19eadfb2efb10c3ab50255e6e5150542d12e24
Louis Gresta <louis.gresta@gmail.com>
2025-02-06 12:26:03 +0100

    test hello_world + generate coverage at build on native env

--- test/desktop/test_hello_world/test_hello_world.c
+++ test/desktop/test_hello_world/test_hello_world.c
@@ -1,20 +1,8 @@
+#include "hello_world.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unity.h>
-
-void hello_world(char* res)
-{
-    char hw[13] = "Hello World\n";
-    char c = hw[0];
-    int i = 0;
-    while (c != '\0') {
-        res[i] = c;
-        i++;
-        c = hw[i];
-    }
-    res[i] = '\0';
-}
 
 void setUp(void)
 {
