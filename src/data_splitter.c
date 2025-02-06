f87852b5d41cf26852e2dcf265a4a10a495a6a39
Movic16 <ibrahim160198@hotmail.com>
2025-02-06 15:13:08 +0100

    mise a jour

--- src/data_splitter.c
+++ src/data_splitter.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
 #include "data_splitter.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
 // Decoupage des donnees
-void dataSplitter(char **stockData, const char *data, const char delimiter, uint16_t *count)
+void dataSplitter(char** stockData, const char* data, const char delimiter, uint16_t* count)
 {
     uint16_t i = 0;
     *count = 0;
@@ -13,19 +13,15 @@ void dataSplitter(char **stockData, const char *data, const char delimiter, uint
     uint16_t post = 0;
     char tempStockData[MAX_DATA];
 
-    for (i = 0; data[i] != caractereFinLigne; i++)
-    {
-        if (data[i] == delimiter)
-        {
+    for (i = 0; data[i] != caractereFinLigne; i++) {
+        if (data[i] == delimiter) {
             tempStockData[post] = caractereFinLigne;
             stockData[*count] = strdup(tempStockData);
 
             (*count)++;
             post = caractereFinLigne;
             tempStockData[0] = caractereFinLigne;
-        }
-        else
-        {
+        } else {
             tempStockData[post] = data[i];
             post++;
         }
