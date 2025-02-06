6fe28c9573abcecc1007de2a59e992f3c1f8717e
Louis Gresta <louis.gresta@gmail.com>
2025-02-06 15:57:30 +0100

    check and build platformio on pre-commit

---
+++ scripts/link_platformio.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+ln -sf ~/.platformio/penv/bin/platformio ~/.local/bin/platformio
+ln -sf ~/.platformio/penv/bin/pio ~/.local/bin/pio
+ln -sf ~/.platformio/penv/bin/piodebuggdb ~/.local/bin/piodebuggdb
