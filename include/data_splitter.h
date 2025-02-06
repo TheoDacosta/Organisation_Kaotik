a81c95c1c0c14ad9aad0cbfe69de55e6e21c3fc0
Movic16 <ibrahim160198@hotmail.com>
2025-02-06 15:10:38 +0100

    Decoupe garde

--- include/data_splitter.h
+++ include/data_splitter.h
@@ -1,3 +1,17 @@
 #ifndef DATA_SPLITTER_H
 
-#endif
+#include <stdio.h>
+#include <stdint.h>
+
+#define MAX_DATA 50
+
+typedef struct dataRadar
+{
+    char data[25];
+
+} TabDataRadar;
+
+// Decoupage des donnees
+void dataSplitter(char **stockData, const char *data, const char delimiter, uint16_t *count);
+
+#endif // DATA_SPLITTER_H
---
+++ test/desktop/test_data_splitter/test_data_splitter.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include "data_splitter.h"
+#include <unity.h>
+
+// Test de decoupage des donnees
+void test_data_splitter()
+{
+    uint16_t count;
+    char *stock_data[10];
+
+    dataSplitter(stock_data, "test1,donnee stocke dit vrais et ok,dating,donnee recu", ',', &count);
+
+    TEST_ASSERT_EQUAL(4, count);
+    TEST_ASSERT_EQUAL_STRING("test1", stock_data[0]);
+
+    TEST_ASSERT_EQUAL_STRING("donnee stocke dit vrais et ok", stock_data[1]);
+    TEST_ASSERT_EQUAL_STRING("dating", stock_data[2]);
+
+    TEST_ASSERT_EQUAL_STRING("donnee recu", stock_data[3]);
+
+    for (uint16_t i = 0; i < count; i++)
+    {
+        free(stock_data[i]);
+    }
+}
