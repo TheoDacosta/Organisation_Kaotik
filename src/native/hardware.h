f617a6c33e7f5475b836a0803b7e47af021754d7
TheoDacosta <theo.dacosta@ynov.com>
2025-02-06 16:16:39 +0100

    instanciation des fonctions de hardware.c dans hardware.h

--- src/native/hardware.h
+++ src/native/hardware.h
@@ -3,6 +3,13 @@
 
 #include <stdio.h>
 
+void srand(unsigned int seed);
+int rand(void);
+int putchar(int c);
+int puts(const char* text);
+char* my_gets(char* str, int size);
+char* itoa(int value, char* str, int base);
 void hardware_init(void);
+
 
 #endif // HARDWARE_H_
