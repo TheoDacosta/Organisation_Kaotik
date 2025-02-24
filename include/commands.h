9982da4a17f4c2c816ccd67cd656ee5ebd0b8043
TheoDacosta <theo.dacosta@ynov.com>
2025-02-24 14:50:55 +0100

    ajout des constantes de jeu

--- include/commands.h
+++ include/commands.h
@@ -8,7 +8,13 @@
 #include <string.h>
 
 #define MAX_COMMAND_SIZE 100
-
+#define MAX_PLANET_ID 65535
+#define MAX_SHIP_ID 9
+#define MAX_ANGLE 359
+#define MAX_COLLECTORS_SPEED 1000
+#define MAX_EXPLORERS_SPEED 2000
+#define MAX_ATTACKERS_SPEED 3000
+#define GAME_MAP_SIZE 19 999
 
 
 void create_move_command(int8_t ship_id, uint16_t angle, uint16_t speed , char* buffer);
