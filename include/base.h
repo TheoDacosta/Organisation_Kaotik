981634b4880f380ebc6f6664f80bc4846fdf5256
Debugsvg <mushsasuske5631@gmail.com>
2025-02-25 09:58:53 +0100

    Ajout base.h

---
+++ include/base.h
@@ -0,0 +1,13 @@
+#ifndef BASE_H
+#define BASE_H
+
+#include <stdint.h>
+#define BASE_POSE_X 0
+#define BASE_POSE_Y 0
+
+typedef struct {
+    uint16_t x;
+    uint16_t y;
+} Base_t;
+
+#endif // BASE_H
