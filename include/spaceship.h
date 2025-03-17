debdbaa28f57979db9ef4c4444d5c101f3ef9f19
Movic16 <ibrahim160198@hotmail.com>
2025-03-17 14:58:52 +0100

    Tire avec le temps ajout commataire

--- include/spaceship.h
+++ include/spaceship.h
@@ -21,6 +21,6 @@ void parse_spaceship(char* data, Spaceship_t* spaceships, uint16_t* nb_spaceship
 
 Spaceship_t* find_spaceship(uint8_t team_id, int8_t ship_id, Spaceship_t* spaceships, uint16_t nb_spaceships);
 
-//
+// Gère le tir du vaisseau en fonction du temps écoulé.
 void shoot_current_timeMs(uint32_t* elapsed_time, uint32_t start_time, uint32_t current_time, Spaceship_t* my_spaceship, Spaceship_t* spaceships, uint16_t nb_spaceships, char* commande);
 #endif // SPACESHIP_H
